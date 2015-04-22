a18dabe653f561b3900e97c5f8111e99084eb929
arturo castro <arturo@openframeworks.cc>
2015-04-22 11:11:37 +0200

    singleton + relative methods + event args + fix pinch to scale
    singleton: in case you want to use the addon from different classes
    avoids to pass a reference around, and it's more similar to how other
    OF event classes work
    
    relative methods: when doing a pinch gesture it's easier to transform
    based on the relative position from the last event, that way you don't
    need the transformations at the beginning of the pinch gesture, only the
    last ones or do relative transformations.
    
    event args: added event args (that internally call the corresponding
    ofxGesture methods) to make the syntax less verbose and more uniform
    with how other OF events work
    
    fix pinch to scale: was reversed, scaling up when closing the fingers
    instead of the opposite

--- src/ofxGestures.h
+++ src/ofxGestures.h
@@ -9,54 +9,76 @@
 
 #include "ofMain.h"
 
+
 class ofxGestures
 {
-    
-private:
-    
-    std::map<int, ofTouchEventArgs> m_touches;
-    
-    bool m_isPanning;
-    ofTouchEventArgs m_panOrigin;
-    ofTouchEventArgs m_panCurrent;
-    
-    bool m_isPinching;
-    ofTouchEventArgs m_pinchOrigin1;
-    ofTouchEventArgs m_pinchOrigin2;
-    ofTouchEventArgs m_pinchCurrent1;
-    ofTouchEventArgs m_pinchCurrent2;
-    
 public:
-    
-    ofxGestures();
-    ~ofxGestures();
+    class PanEvent{
+    public:
+        ofVec2f getOrigin() const;
+        ofVec2f getDelta() const;
+    };
 
-private:
-    
-    void touchDown(ofTouchEventArgs & touch);
-    void touchMoved(ofTouchEventArgs & touch);
-    void touchUp(ofTouchEventArgs & touch);
-    
-    bool touchExists(int touchNum){return (m_touches.find(touchNum) != m_touches.end());}
+    ofEvent<PanEvent> panGestureEvent;
+    ofEvent<PanEvent> panGestureEndedEvent;
 
-public:
+    class PinchEvent{
+    public:
+        ofVec2f getOrigin() const;
+        ofVec3f getPrevious() const;
+        ofVec2f getDelta() const;
+        ofVec2f getRelativeDelta() const;
+        double getAngle() const;
+        double getScale() const;
+        double getRelativeAngle() const;
+        double getRelativeScale() const;
+    };
     
-    ofEvent<void> panGestureEvent;
-    ofEvent<void> panGestureEndedEvent;
+    ofEvent<PinchEvent> pinchGestureEvent;
+    ofEvent<PinchEvent> pinchGestureEndedEvent;
     
-    ofEvent<void> pinchGestureEvent;
-    ofEvent<void> pinchGestureEndedEvent;
-    
-public:
-    
+    static ofxGestures & get();
     bool isPanning() const {return m_isPanning;}
     ofVec2f getPanOrigin() const;
     ofVec2f getPanDelta() const;
     
     bool isPinching() const {return m_isPinching;}
     ofVec2f getPinchOrigin() const;
+    ofVec2f getPinchPrevious() const;
     ofVec2f getPinchDelta() const;
+    ofVec2f getPinchRelativeDelta() const;
     double getPinchAngle() const;
+    double getPinchRelativeAngle() const;
     double getPinchScale() const;
+    double getPinchRelativeScale() const;
+
+private:
+    bool touchDown(ofTouchEventArgs & touch);
+    bool touchMoved(ofTouchEventArgs & touch);
+    bool touchUp(ofTouchEventArgs & touch);
+
+    bool touchExists(int touchNum){return (m_touches.find(touchNum) != m_touches.end());}
+
+    std::map<int, ofTouchEventArgs> m_touches;
+
+    bool m_isPanning;
+    ofTouchEventArgs m_panOrigin;
+    ofTouchEventArgs m_panCurrent;
+
+    bool m_isPinching;
+    ofTouchEventArgs m_pinchOrigin1;
+    ofTouchEventArgs m_pinchOrigin2;
+    ofTouchEventArgs m_pinchPrevious1;
+    ofTouchEventArgs m_pinchPrevious2;
+    ofTouchEventArgs m_pinchCurrent1;
+    ofTouchEventArgs m_pinchCurrent2;
+
+    PinchEvent pinch;
+    PanEvent pan;
+
+    ofxGestures();
+
+public:
+    ~ofxGestures();
 };
 
